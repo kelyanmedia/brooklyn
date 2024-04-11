6b27d6f7dc1c8afa64414d86ef2572e0f39af83e
Dima <uvarov3123@gmail.com>
2024-04-11 14:07:51 +0300

    add cards

--- src/js/files/isotope.js
+++ src/js/files/isotope.js
@@ -3,19 +3,19 @@
 */
 
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile, FLS } from "./functions.js";
+import { isMobile, FLS } from "./functions.js"
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from "./modules.js"
 
-import Isotope from 'isotope-layout/js/isotope.js';
+import Isotope from "isotope-layout/js/isotope.js"
 
-const items = document.querySelector('[data-iso-items]');
+const items = document.querySelector("[data-iso-items]")
 if (items) {
-	const itemsGrid = new Isotope(items, {
-		itemSelector: '[data-iso-item]',
-		masonry: {
-			fitWidth: true,
-			gutter: 20
-		}
-	});
+  const itemsGrid = new Isotope(items, {
+    itemSelector: "[data-iso-item]",
+    masonry: {
+      //   fitWidth: true,
+      gutter: 20,
+    },
+  })
 }
